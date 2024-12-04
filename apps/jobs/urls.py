da4dfe7cfb576f40e5405ffe9b58758ccbf683c6
B1UG <noor.feraas@gmail.com>
2024-12-04 20:37:36 +0300

    changed

--- apps/jobs/urls.py
+++ apps/jobs/urls.py
@@ -6,12 +6,12 @@ from .views import (
 )
 
 urlpatterns = [
-    path('job-posts/', JobPostListCreateView.as_view(), name='jobpost-list-create'),
-    path('job-posts/<int:pk>/', JobPostDetailView.as_view(), name='jobpost-detail'),
+    path('job_posts/', JobPostListCreateView.as_view(), name='jobpost_list_create'),
+    path('job_posts/<int:pk>/', JobPostDetailView.as_view(), name='jobpost_detail'),
 
-    path('applications/', ApplicationListCreateView.as_view(), name='application-list-create'),
-    path('applications/<int:pk>/', ApplicationDetailView.as_view(), name='application-detail'),
+    path('applications/', ApplicationListCreateView.as_view(), name='application_list_create'),
+    path('applications/<int:pk>/', ApplicationDetailView.as_view(), name='application_detail'),
 
-    path('saved-jobs/', SavedJobListCreateView.as_view(), name='savedjob-list-create'),
-    path('saved-jobs/<int:pk>/', SavedJobDetailView.as_view(), name='savedjob-detail'),
+    path('saved_jobs/', SavedJobListCreateView.as_view(), name='savedjob-list_create'),
+    path('saved_jobs/<int:pk>/', SavedJobDetailView.as_view(), name='savedjob_detail'),
 ]
